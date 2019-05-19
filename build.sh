e18c604a49857a65371bec8924737157e6b15e57
Fork TIPS <jojapoppa@protonmail.com>
2019-05-19 20:48:34 +0000

    comments about alpine...

--- build.sh
+++ build.sh
@@ -5,6 +5,21 @@ mkdir dist
 mkdir build
 cp ./src/assets/icon.* ./build/
 
+# I was never able to get electron-builder to work from Alpine Linux
+#   because support for wine32 is weak on Alpine, and that is currently
+#   required to create windows compatible instrallers (which is required)
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib:/usr/lib
+# sudo apk --no-cache add ca-certificates wget
+# sudo wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
+# wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.28-r0/glibc-2.28-r0.apk
+# sudo apk add glibc-2.28-r0.apk
+# npm install electron-builder
+# ln -s /lib/libz.so.1 /usr/lib/
+# sudo apk add gtk+3.0
+# sudo apk add alsa-lib-dev
+# sudo npm i pkg -g
+# ln -s /usr/bin/wine64 /usr/bin/wine
+
 set DEBUG=electron-builder
 
 rm -r bin
